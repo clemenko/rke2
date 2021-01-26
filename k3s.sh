f1687e5caa9fb1cb964b1098d38a957e5c4fc8a9
Andy Clemenko <clemenko@gmail.com>
2021-01-26 16:03:11 -0500

    cleanup

--- k3s.sh
+++ k3s.sh
@@ -108,6 +108,57 @@ if [[ "$image" = *"centos"* ]]; then
   echo "$GREEN" "ok" "$NORMAL"
 fi
 
+#kernel tuning
+echo -n " updating kernel settings "
+pdsh -l $user -w $host_list 'cat << EOF >> /etc/sysctl.conf
+# SWAP settings
+vm.swappiness=0
+vm.overcommit_memory=1
+
+# Have a larger connection range available
+net.ipv4.ip_local_port_range=1024 65000
+
+# Increase max connection
+net.core.somaxconn = 10000
+
+# Reuse closed sockets faster
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=15
+
+# The maximum number of "backlogged sockets".  Default is 128.
+net.core.somaxconn=4096
+net.core.netdev_max_backlog=4096
+
+# 16MB per socket - which sounds like a lot,
+# but will virtually never consume that much.
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+
+# Various network tunables
+net.ipv4.tcp_max_syn_backlog=20480
+net.ipv4.tcp_max_tw_buckets=400000
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_syn_retries=2
+net.ipv4.tcp_synack_retries=2
+net.ipv4.tcp_wmem=4096 65536 16777216
+
+# ARP cache settings for a highly loaded docker swarm
+net.ipv4.neigh.default.gc_thresh1=8096
+net.ipv4.neigh.default.gc_thresh2=12288
+net.ipv4.neigh.default.gc_thresh3=16384
+
+# ip_forward and tcp keepalive for iptables
+net.ipv4.tcp_keepalive_time=600
+net.ipv4.ip_forward=1
+
+# monitor file system events
+fs.inotify.max_user_instances=8192
+fs.inotify.max_user_watches=1048576
+EOF
+sysctl -p' > /dev/null 2>&1
+echo "$GREEN" "ok" "$NORMAL"
+
 #or deploy k3s
 if [ "$orchestrator" = k3s ]; then
   echo -n " deploying k3s "
@@ -115,7 +166,7 @@ if [ "$orchestrator" = k3s ]; then
 
   for workeri in $(awk '{print $1}' hosts.txt |sed 1d); do 
     k3sup join --ip $workeri --server-ip $server --user $user --k3s-channel $k3s_channel > /dev/null 2>&1
-#    rsync -avP ~/.kube/config $user@$workeri:/opt/kube_config > /dev/null 2>&1
+    rsync -avP ~/.kube/config $user@$workeri:/opt/kube_config > /dev/null 2>&1
   done 
 
   echo "$GREEN" "ok" "$NORMAL"
@@ -135,6 +186,47 @@ if [ "$orchestrator" = rke ]; then
   rsync -avP $user@$server:/etc/rancher/rke2/rke2.yaml ~/.kube/config
   sed -i'' -e "s/127.0.0.1/$server/g" ~/.kube/config
 
+  echo "$GREEN" "ok" "$NORMAL"
+fi
+
+#deploy Rancher
+if [ "$orchestrator" = rancher ]; then
+  echo -n " starting rancher server "
+  ssh $user@$server "docker run -d -p 80:80 -p 443:443 --privileged --restart=unless-stopped rancher/rancher" > /dev/null 2>&1
+
+  until curl $server:443 > /dev/null 2>&1; do echo -n .; sleep 2; done
+  echo "$GREEN" "ok" "$NORMAL"
+
+  echo -n " setting up rancher server "
+  until [ "$token" != "" ] && [ "$token" != null ]; do 
+    token=$(curl -sk https://$server/v3-public/localProviders/local?action=login -H 'content-type: application/json' -d '{"username":"admin","password":"admin"}'| jq -r .token) > /dev/null 2>&1
+  done
+
+  curl -sk https://$server/v3/users?action=changepassword -H 'content-type: application/json' -H "Authorization: Bearer $token" -d '{"currentPassword":"admin","newPassword":"'$password'"}'  > /dev/null 2>&1 
+  
+  api_token=$(curl -sk https://$server/v3/token -H 'content-type: application/json' -H "Authorization: Bearer $token" -d '{"type":"token","description":"automation"}' | jq -r .token)
+  echo $api_token > api_token
+  
+  curl -sk https://$server/v3/settings/server-url -H 'content-type: application/json' -H "Authorization: Bearer $api_token" -X PUT -d '{"name":"server-url","value":"https://'$server'"}'  > /dev/null 2>&1
+  
+  curl -sk https://$server/v3/settings/telemetry-opt -X PUT -H 'content-type: application/json' -H 'accept: application/json' -H "Authorization: Bearer $api_token" -d '{"value":"out"}' > /dev/null 2>&1
+  echo "$GREEN" "ok" "$NORMAL"
+
+  echo -n " attaching agents "
+  agent_list=$(sed -n 2,"$num"p hosts.txt|awk '{printf $1","}')
+
+  # Create cluster
+  clusterid=$(curl -sk https://$server/v3/cluster -H 'content-type: application/json' -H "Authorization: Bearer $api_token" -d '{"type":"cluster","nodes":[],"rancherKubernetesEngineConfig":{"ignoreDockerVersion":true},"name":"rancher"}' | jq -r .id )
+
+  # Generate token (clusterRegistrationToken) and extract nodeCommand
+  agent_command=$(curl -sk https://$server/v3/clusterregistrationtoken -H 'content-type: application/json' -H "Authorization: Bearer $api_token" --data-binary '{"type":"clusterRegistrationToken","clusterId":"'$clusterid'"}' | jq -r .nodeCommand)
+
+  ssh $user@$server "$agent_command --etcd --controlplane --worker" > /dev/null 2>&1
+  pdsh -l $user -w $agent_list "$agent_command --worker" > /dev/null 2>&1
+  echo "$GREEN" "ok" "$NORMAL"
+
+  echo -n " setting up kubectl "
+  curl -sk https://$server/v3/clusters/$clusterid?action=generateKubeconfig -X POST -H 'accept: application/json' -H "Authorization: Bearer $api_token" | jq -r .config > ~/.kube/config
   echo "$GREEN" "ok" "$NORMAL"
 fi
 
@@ -238,25 +330,25 @@ function demo () {
   echo "$GREEN""ok" "$NORMAL"
 
   echo -n "  - prometheus/grafana "
-  kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/prometheus.yml > /dev/null 2>&1
-  kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/kube-state-metrics-complete.yml > /dev/null 2>&1
-  kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/prometheus_grafana_dashboards.yml > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/prometheus.yml > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/kube-state-metrics-complete.yml > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/prometheus/prometheus_grafana_dashboards.yml > /dev/null 2>&1
   echo "$GREEN""ok" "$NORMAL"
 
   echo -n "  - patching stackrox for prometheus "
-  kubectl -n stackrox patch svc/sensor -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}, "metadata":{"annotations":{"prometheus.io.scrape": "true", "prometheus.io/port": "9090"}}}' > /dev/null 2>&1
+  #kubectl -n stackrox patch svc/sensor -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}, "metadata":{"annotations":{"prometheus.io.scrape": "true", "prometheus.io/port": "9090"}}}' > /dev/null 2>&1
 
-  kubectl -n stackrox patch svc/central -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}, "metadata":{"annotations":{"prometheus.io.scrape": "true", "prometheus.io/port": "9090"}}}' > /dev/null 2>&1
+  #kubectl -n stackrox patch svc/central -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}, "metadata":{"annotations":{"prometheus.io.scrape": "true", "prometheus.io/port": "9090"}}}' > /dev/null 2>&1
 
   # Modify network policies to allow ingress
   kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/stackrox_prometheus.yml > /dev/null 2>&1
   echo "$GREEN""ok" "$NORMAL"
 
   echo -n "  - openfaas "
-  kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml > /dev/null 2>&1
-  kubectl -n openfaas create secret generic basic-auth --from-literal=basic-auth-user=admin --from-literal=basic-auth-password="$password" > /dev/null 2>&1
-  kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/openfass.yml > /dev/null 2>&1
-  kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/openfaas_traefik.yml  > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml > /dev/null 2>&1
+  #kubectl -n openfaas create secret generic basic-auth --from-literal=basic-auth-user=admin --from-literal=basic-auth-password="$password" > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/openfass.yml > /dev/null 2>&1
+  #kubectl apply -f https://raw.githubusercontent.com/clemenko/k8s_yaml/master/openfaas_traefik.yml  > /dev/null 2>&1
   echo "$GREEN""ok" "$NORMAL"
 
   echo -n "  - harbor "
@@ -313,6 +405,7 @@ function usage () {
   echo " ./k3s.sh status # get vm status"
   echo " ./k3s.sh demo # deploy demo apps"
   echo " ./k3s.sh full # full send"
+  echo " ./k3s.sh simple # just the rox"
   echo ""
   echo "-------------------------------------------------"
   echo ""
@@ -326,5 +419,6 @@ case "$1" in
         rox) rox;;
         demo) demo;;
         full) full;;
+        simple) up && rox;;
         *) usage;;
 esac
