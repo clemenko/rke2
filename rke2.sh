d943a568155e9d00f39fac40590122b710a7a49f
Andy Clemenko <clemenko@gmail.com>
2024-01-12 08:24:59 -0500

    updated rancher func

--- rke2.sh
+++ rke2.sh
@@ -185,7 +185,7 @@ case "$1" in
         dolist) dolist;;
         keycloak) keycloak;;
         longhorn) longhorn;;
-        rancher) rancher;;
+        rancher) rancher && longhorn && neu;;
         demo) demo;;
         fleet) fleet;;
         rox) rox;;
