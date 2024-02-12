e766367efdce26a86557af4579fd60e79e22f151
Andy Clemenko <clemenko@gmail.com>
2024-02-12 09:39:44 -0500

    versions and something else.

--- rke2.sh
+++ rke2.sh
@@ -185,7 +185,7 @@ case "$1" in
         dolist) dolist;;
         keycloak) keycloak;;
         longhorn) longhorn;;
-        rancher) rancher && neu;;
+        rancher) rancher;;
         demo) demo;;
         fleet) fleet;;
         rox) rox;;
